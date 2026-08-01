bb9017010e2aae67a6043e6f0614af4555959663
raxa177 <raxachauhan@paytouch.in>
2026-08-01 13:04:59 +0530

    B2C-51 implemented pagination

--- app/src/main/java/com/shreefintech/paytouchconsumer/retrofit/model/electricity/ElectricityTransactionStatusRequest.kt
+++ app/src/main/java/com/shreefintech/paytouchconsumer/retrofit/model/electricity/ElectricityTransactionStatusRequest.kt
@@ -3,5 +3,7 @@ package com.shreefintech.paytouchconsumer.retrofit.model.electricity
 import com.google.gson.annotations.SerializedName
 
 data class ElectricityTransactionStatusRequest(
-    @field:SerializedName("transaction_id") val transactionId: String?
+    @field:SerializedName("transaction_id") val transactionId: String?,
+    @field:SerializedName("page")           val page: Int?,
+    @field:SerializedName("per_page")       val perPage: Int?
 )
