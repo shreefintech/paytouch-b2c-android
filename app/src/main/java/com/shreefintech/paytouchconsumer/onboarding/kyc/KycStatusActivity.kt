3aca1e50999315e7112078a176c09243992c9ba3
raxa177 <raxachauhan@paytouch.in>
2026-08-08 15:08:10 +0530

    B2C-87

--- app/src/main/java/com/shreefintech/paytouchconsumer/onboarding/kyc/KycStatusActivity.kt
+++ app/src/main/java/com/shreefintech/paytouchconsumer/onboarding/kyc/KycStatusActivity.kt
@@ -5,9 +5,10 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.shreefintech.paytouchconsumer.BaseActivity
 import com.shreefintech.paytouchconsumer.R
 
-class KycStatusActivity : AppCompatActivity() {
+class KycStatusActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
