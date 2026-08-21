f1611fca04e569b5f2a8584d732fc6f193dc5d3d
raxa177 <raxachauhan@paytouch.in>
2026-08-21 13:30:13 +0530

    B2C-87 review changes done

--- app/src/main/java/com/shreefintech/paytouchconsumer/loadwallet/LoadWalletActivity.kt
+++ app/src/main/java/com/shreefintech/paytouchconsumer/loadwallet/LoadWalletActivity.kt
@@ -299,6 +299,7 @@ class LoadWalletActivity : BaseActivity() {
             },
             onError = { msg ->
                 ToastUtil.showDelete(mActivity, msg)
+                updateEmptyState()
             }
         )
     }
